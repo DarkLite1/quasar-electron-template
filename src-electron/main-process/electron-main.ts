e7329c8865aded507893a38a2db4d700cf9dc4d1
DarkLite1 <BrechtGijbels@gmail.com>
2021-03-19 11:24:53 +0100

    use !! for boolean conversion

--- src-electron/main-process/electron-main.ts
+++ src-electron/main-process/electron-main.ts
@@ -33,10 +33,8 @@ async function createWindow() {
     webPreferences: {
       // Change from /quasar.conf.js > electron > nodeIntegration;
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
-      nodeIntegration:
-        process.env.QUASAR_NODE_INTEGRATION?.toLowerCase() === 'true',
-      nodeIntegrationInWorker:
-        process.env.QUASAR_NODE_INTEGRATION?.toLowerCase() === 'true'
+      nodeIntegration: !!process.env.QUASAR_NODE_INTEGRATION,
+      nodeIntegrationInWorker: !!process.env.QUASAR_NODE_INTEGRATION
 
       // More info: /quasar-cli/developing-electron-apps/electron-preload-script
       // preload: path.resolve(__dirname, 'electron-preload.js')
